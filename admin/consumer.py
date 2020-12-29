820f1e58f0c96af043e3648f9fd4180ec15fcaa1
PanGan21 <ganelis.pan@gmail.com>
2020-12-29 21:25:00 +0100

    event-bus: queue containers for consumers

--- admin/consumer.py
+++ admin/consumer.py
@@ -15,7 +15,8 @@ def callback(ch, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(
+    queue='admin', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
--- main/consumer.py
+++ main/consumer.py
@@ -15,7 +15,8 @@ def callback(ch, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(
+    queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
